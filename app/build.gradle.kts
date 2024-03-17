8bb6a7fbcfdbe30d32e26a5d9bcdc7fcfa45a284
Reza Najafi <mrnj92@gmail.com>
2024-03-17 04:54:46 +0100

    Removes unused files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/picsum/MainActivity.kt
+++ app/src/main/java/com/example/picsum/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.picsum.ui.theme.PicsumTheme
 import com.example.screens.PhotosListScreen
 import com.slack.circuit.backstack.rememberSaveableBackStack
@@ -25,21 +21,5 @@ class MainActivity : ComponentActivity() {
                 NavigableCircuitContent(navigator, backStack)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PicsumTheme {
-        Greeting("Android")
     }
 }
